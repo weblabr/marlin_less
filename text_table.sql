8b4b34948b5009a0df27a17715597711ff05671c
WebLabr <psdtocms@gmail.com>
2020-09-13 01:55:36 +0300

    add task 9

---
+++ text_table.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 13 2020 г., 01:54
+-- Версия сервера: 10.3.22-MariaDB-log
+-- Версия PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `marlin_less`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `text_table`
+--
+
+CREATE TABLE `text_table` (
+  `text_field` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `text_table`
+--
+
+INSERT INTO `text_table` (`text_field`) VALUES
+(' Пример Текста '),
+(' Пример Текста '),
+('  ');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
